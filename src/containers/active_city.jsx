850fdf4985022f77af1045cb0d856deecf2e1840
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2019-12-20 16:16:21 +0900

    active_city.jsx added with only react

---
+++ src/containers/active_city.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ActiveCity = (props) => {
+  if (!props.activeCity) {
+    return (
+      <div className="active-city">
+        <p>Select a city...</p>
+      </div>
+    );
+  }
+
+  const url = `https://kitt.lewagon.com/placeholder/cities/${props.activeCity.slug}`;
+
+  return (
+    <div className="active-city">
+      <h3>{props.activeCity.name}</h3>
+      <p>{props.activeCity.address}</p>
+      <img src={url} width="100%" alt="img for the selected city" />
+    </div>
+  );
+};
+
+export default ActiveCity;
